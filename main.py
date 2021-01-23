a6e3363ad6ffc783e68c192401c3a0c586665e1e
varik <varikakasandor@gmail.com>
2021-01-23 21:26:17 +0100

    Processing function added

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, request, url_for, render_template
+import json
+from model.py import process_user
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route("/process",methods=["POST"])
+def process():
+    IDs =  request.form['IDs']
+    add_to_model=request.form['add_to_model']
+    posts=[]
+    for social, id in IDs.items():
+        curr_weight,curr_text=get_user_text(social,id)
+        posts.append((curr_weight,curr_text))
+    close_ids,far_ids=process_user(IDs,posts,add_to_model)
+    res={"close_ids":close_ids,"far_ids":far_ids}
+    return json.dumps(res)
