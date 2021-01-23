9abcff1f838ff599f7e304271d5c91c1461ad0b5
Skye <aidantpurchase@gmail.com>
2021-01-23 20:42:47 +0000

    Better form submission

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,18 @@
 $(function() {
+    $('#close_ids').hide();
+    $('#far_ids').hide();
     $('button').click(function() {
         var ID = $('#inputTwitterID').val();
+        var social = $('#social').val();
         $.ajax({
             url: '/process',
-            data: {'twitter': ID},
+            data: {social: ID},
             type: 'POST'
         }).done(function(data) {
-            $('#close_ids').text(data.close_ids).show();
-            $('#far_ids').text(data.far_ids).show();
+            $('#close_ids').show();
+            $('#far_ids').show();
+            $('#close_id').text(data.close_ids).show();
+            $('#far_id').text(data.far_ids).show();
         });
     });
 })
