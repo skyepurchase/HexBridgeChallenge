f88c8a6deef1ea967dcb9d0b0657114ca534b636
Gabor Pituk <pitukgabor@gmail.com>
2021-01-24 00:11:08 +0100

    In-memory state instead of db

--- astra.py
+++ astra.py
@@ -8,6 +8,13 @@ from cassandra.auth import PlainTextAuthProvider
 from scipy.sparse.csr import csr_matrix
 
 
+storage = [
+        ({}, csr_matrix([1, 0, 0, 0, 0, 0, 0, 0])),
+        ({}, csr_matrix([0, 0, 1, 0, 0, 0, 0, 0]))
+]
+
+
+
 def run_command(cmd):
     cloud_config = {
             'secure_connect_bundle': './secure-connect-hexbridgechallenge.zip'
@@ -29,15 +36,20 @@ def test_connection():
 
 
 def save_user(twitter_id, data):
-    data_string = base64.b64encode(pickle.dumps(data)).decode("ascii")
-    run_command(f"INSERT INTO \"HexBridge\".wordcounts (id, pickle) VALUES('{twitter_id}', textAsBlob('{data_string}'))")
+    #data_string = base64.b64encode(pickle.dumps(data)).decode("ascii")
+    #run_command(f"INSERT INTO \"HexBridge\".wordcounts (id, pickle) VALUES('{twitter_id}', textAsBlob('{data_string}'))")
+    storage.append((twitter_id, data))
 
 
 def load_user(twitter_id):
-    row = run_command(f"select pickle from \"HexBridge\".wordcounts where id = '{twitter_id}';")
-    data = pickle.loads(base64.b64decode(row[0]))
+    #row = run_command(f"select pickle from \"HexBridge\".wordcounts where id = '{twitter_id}';")
+    #data = pickle.loads(base64.b64decode(row[0]))
     #print(data)
-    return data
+    #return data
+    pass
+
+def load_all_users():
+    return storage
 
 if __name__ == "__main__":
     test_connection()
