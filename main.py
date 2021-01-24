9bc09e493e29767a3af9eeb1f1e7a6a31da37cfc
varik <varikakasandor@gmail.com>
2021-01-24 10:21:48 +0100

    Added dummy exception handling

--- main.py
+++ main.py
@@ -9,20 +9,22 @@ app = Flask(__name__)
 
 @app.route('/',methods=["GET"])
 def root():
-    print("Call to root")
     return render_template('index.html')
 
 
 @app.route("/process",methods=["POST"])
 def process():
-    social=request.form['social']
-    consent=request.form['consent']
-    id=request.form['ID']
-    scraper = Scraper()
-    posts=scraper.get_user_text(Social.REDDIT,id) if (social=="reddit") else scraper.get_user_text(Social.TWITTER,id)
-    model=Model()
-    IDs={}
-    IDs[social]=id
-    close_ids,far_ids=model.process_user(IDs,posts,consent)
-    res={"close_ids":close_ids,"far_ids":far_ids}
-    return json.dumps(res)
+    try:
+        social=request.form['social']
+        consent=request.form['consent']
+        id=request.form['ID']
+        scraper = Scraper()
+        posts=scraper.get_user_text(Social.REDDIT,id) if (social=="reddit") else scraper.get_user_text(Social.TWITTER,id)
+        model=Model()
+        IDs={}
+        IDs[social]=id
+        close_ids,far_ids=model.process_user(IDs,posts,consent)
+        res={"close_ids":close_ids,"far_ids":far_ids}
+        return json.dumps(res)
+    except Exception as e:
+        return json.dumps({"close_ids":[],"far_ids":[]})
