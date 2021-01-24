25c5af6592809e91ac8258d54b51c40b12504ad4
Skye <aidantpurchase@gmail.com>
2021-01-24 00:02:33 +0000

    Added ability for both IDs

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,24 @@
 $(function() {
     $('#close_id_card').hide();
     $('#far_id_card').hide();
+
+    let both = false;
+    let select = document.getElementById('social');
+    select.addEventListener('change', e => {
+        if (e.target.value === 'both') {
+            $('#idForm').append('<input type="text" id="inputID2" name="ID" class="col form-control" placeholder="Your ID" required/>')
+            both = true;
+        } else {
+            let ID2el = document.getElementById('inputID2');
+            ID2el.parentNode.removeChild(ID2el); // Such a weird wat of doing it but it works
+        }
+    })
+
     $('form').on("submit", function(e) {
         const ID = $('#inputID').val();
+        if(both) {
+            const ID2 = $('#inputID2').val();
+        }
 
         if (ID) {
             const social = $('#social').val();
@@ -15,6 +31,8 @@ $(function() {
                 case "reddit":
                     senddata = {'consent': consent, 'reddit': ID};
                     break;
+                case "both":
+                    senddata = {'consent': consent, 'reddit':ID, 'twitter':ID2}
                 default:
                     senddata = {'consent': consent};
             }
