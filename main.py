e5c778eb56a1c794d3d446447fa2ba12b6f042a9
varik <varikakasandor@gmail.com>
2021-01-24 02:37:41 +0100

    remove unnecessary lines

--- main.py
+++ main.py
@@ -19,11 +19,10 @@ def process():
     consent=request.form['consent']
     id=request.form['ID']
     scraper = Scraper()
-    curr_weight,curr_text=scraper.get_user_text(Social.REDDIT,id) if (social=="reddit") else scraper.get_user_text(Social.TWITTER,id)
+    posts=scraper.get_user_text(Social.REDDIT,id) if (social=="reddit") else scraper.get_user_text(Social.TWITTER,id)
     model=Model()
     IDs={}
     IDs[social]=id
-    posts=[(curr_weight,curr_text)]
     close_ids,far_ids=model.process_user(IDs,posts,consent)
     res={"close_ids":close_ids,"far_ids":far_ids}
     return json.dumps(res)
