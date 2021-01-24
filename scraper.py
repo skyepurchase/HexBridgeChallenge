71eca90db3dcf47ac3f116120ea8740893d02621
nikgeopro <nikola.georgiev2000@hotmail.com>
2021-01-24 12:21:14 +0100

    Better error messages.

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import enum
 import praw
+import prawcore
+from prawcore.exceptions import PrawcoreException
 import tweepy
 import configparser
 
@@ -28,35 +30,69 @@ class Scraper:
 
     def get_user_text(self, social_source: Social, name: str, item_count=10):
         if(social_source == Social.TWITTER):
-            friends = self.get_twitter_friends(name)
-            texts = [self.get_user_comments(
-                social_source, f, item_count=3) for f in friends]
             comm = self.get_user_comments(
                 social_source, name)
+            friends = self.get_twitter_friends(name)
+            texts = []
+            for f in friends:
+                try:
+                    texts.append(self.get_user_comments(
+                        social_source, f, item_count=3))
+                except Exception:
+                    continue
             return [(5, comm), (1, " ".join(texts))]
         elif(social_source == Social.REDDIT):
             return [(1, self.get_user_comments(social_source, name, item_count))]
+        else:
+            raise Exception(
+                f"Incorrect social_source. It needs to be one of Social.{', Social.'.join(Social._member_names_)}.")
 
     def get_user_comments(self, social_source: Social, name: str, item_count=10):
         if(social_source == Social.TWITTER):
-            timeline = self.twitter.user_timeline
-            comments = [t.text for t in tweepy.Cursor(timeline,
-                                                      screen_name=name, count=item_count).items(item_count)]
+            try:
+                timeline = self.twitter.user_timeline
+                comments = [t.text for t in tweepy.Cursor(timeline,
+                                                          screen_name=name, count=item_count).items(item_count)]
+                return " ".join(comments)
+            except TweepError as e:
+                code = e.response.status_code
+                if(code == 404):
+                    raise Exception("User not found.")
+                elif(code == 401):
+                    raise Exception(
+                        "Your account might be private. We are not authorized to access your timeline/friends.")
+                elif(code == 429):
+                    raise Exception(
+                        "Too many users! Wait 15 minutes before retrying as Twitter needs to reset our quota.")
+                else:
+                    raise e
         elif(social_source == Social.REDDIT):
-            redditor = self.reddit.redditor(name=name)
-            comments = [t.body for t in redditor.comments.top(
-                "all", limit=item_count)]
-        return " ".join(comments)
+            try:
+                redditor = self.reddit.redditor(name=name)
+                comments = [t.body for t in redditor.comments.top(
+                    "all", limit=item_count)]
+                return " ".join(comments)
+            except PrawcoreException as e:
+                code = e.response.status_code
+                if(code == 404):
+                    raise Exception("User not found.")
+                elif(code == 401):
+                    raise Exception(
+                        "Your account might be private. We cannot access your Reddit history.")
+                elif(code == 429):
+                    raise Exception(
+                        "Too many requests! Wait 5 minutes before retrying as Reddit needs to reset our quota.")
+                else:
+                    raise e
+        else:
+            raise Exception(
+                f"Incorrect social_source. It needs to be one of Social.{', Social.'.join(Social._member_names_)}.")
 
     def get_twitter_friends(self, name: str, limit=5):
-        try:
-            friends = self.twitter.friends_ids(screen_name=name)[:limit]
-            friend_names = [self.twitter.get_user(
-                id).screen_name for id in friends]
-            return friend_names
-        except TweepError as e:
-            print(e)
-            return []
+        friends = self.twitter.friends_ids(screen_name=name)[:limit]
+        friend_names = [self.twitter.get_user(
+            id).screen_name for id in friends]
+        return friend_names
 
     def get_twitter_followers(self, name: str, limit=10):
         followers = self.twitter.followers_ids(screen_name=name)[:limit]
@@ -71,3 +107,25 @@ class Scraper:
             names = [post.author.name for post in self.reddit.subreddit(
                 "news").comments(limit=limit)]
         return names
+
+
+scraper = Scraper()
+# try:
+#     try:
+#         print(scraper.get_user_text(Social.TWITTER, "nikgeo25"))
+#     except PrawcoreException as e:
+#         print(vars(e.response))
+#         code = e.response.status_code
+#         if(code == 404):
+#             raise Exception("No such user.")
+#         elif(code == 401):
+#             raise Exception(
+#                 "Your account might be private. We cannot access your timeline.")
+#         elif(code == 429):
+#             raise Exception(
+#                 "Too many users! Wait 15 minutes before retrying as Twitter needs to reset our quota.")
+#         else:
+#             raise e
+# except Exception as e:
+#     print(e)
+print(scraper.get_user_text("N", "olololoajd"))
